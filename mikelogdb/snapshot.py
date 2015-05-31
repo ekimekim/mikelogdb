d2b2ca6ffe7b7455d7b4f5211f2c898d24eba6fa
Mike Lang <mikelang3000@gmail.com>
2015-05-31 09:45:17 -0700

    snapshot: Implement transaction creation from data diff
    Also replace tracking tid with tracking the whole previous transaction.
    This is so we also know the hash.

--- mikelogdb/snapshot.py
+++ mikelogdb/snapshot.py
@@ -1,7 +1,8 @@
 
 from copy import deepcopy
 
-from util import EMPTY, json_dump, json_dump_pretty
+from util import EMPTY, json_load, json_dump, json_dump_pretty
+from transaction import Transaction
 
 
 class Snapshot(object):
@@ -9,17 +10,22 @@ class Snapshot(object):
 	Snapshots in general should be immutable. Exercise caution handing the data
 	to users which might modify it. Note that all copies are deep copies for this reason."""
 
-	def __init__(self, tid=None, data=None):
-		"""data is initial state. tid may be None to represent no starting point; the first
-		transaction applied MUST be tid 0."""
-		self.tid = tid
+	def __init__(self, transaction=None, data=None):
+		"""data is initial state. transaction may be None to represent no starting point; the first
+		transaction applied MUST be tid 0. Otherwise transaction is the most recently applied transaction."""
+		self.transaction = transaction
 		if data is None:
 			self.data = {}
 		else:
 			self.data = deepcopy(data)
 
+	@property
+	def tid(self):
+		"""The tid we are up to."""
+		return self.transaction.tid if self.transaction is not None else None
+
 	def copy(self):
-		return Snapshot(self.tid, self.data)
+		return Snapshot(self.transaction, self.data)
 
 	def apply(self, transaction):
 		"""Apply the transaction to this snapshot, returning a new snapshot."""
@@ -32,7 +38,7 @@ class Snapshot(object):
 				raise ValueError("Attempted to apply tid {} to a snapshot at tid {}".format(
 				                 transaction.tid, self.tid))
 
-		result = Snapshot(transaction.tid, self.data)
+		result = Snapshot(transaction, self.data)
 
 		for path, old, new in transaction.actions:
 			path, path_end = path[:-1], path[-1]
@@ -55,17 +61,60 @@ class Snapshot(object):
 			except (KeyError, IndexError):
 				raise ValueError("Error applying transaction {!r}: root{} has no key {}".format(
 				                 transaction, prefix_str(), key))
+			except TypeError as e:
+				raise ValueError("Error applying transaction {!r}: TypeError while changing key {}[{}]: {}".format(
+				                 transaction, prefix_str(), key, e))
 
 		return result
 
 	def to_data(self):
-		return dict(tid=self.tid, data=self.data)
+		return dict(transaction=self.transaction.to_data(), data=self.data)
 
 	def json(self):
 		return json_dump(self.to_data())
 
+	@classmethod
+	def from_str(cls, s):
+		data = json_load(s)
+		t = Transaction(**data['transaction'])
+		return cls(t, data['data'])
+
 	def __repr__(self):
-		return "<{} tid={}>".format(type(self).__name__, self.tid)
+		return "<{} of {!r}>".format(type(self).__name__, self.transaction)
 
 	def __str__(self):
 		return json_dump_pretty(self.to_data())
+
+	def commit(self, initial, parent=None):
+		"""Helper method that creates a transaction going from given initial snapshot to this snapshot,
+		and returns (transaction, new_snapshot) where new_snapshot correctly records transaction
+		as the most recent transaction."""
+		t = self.diff(initial, parent)
+		s = Snapshot(t, self.data)
+		return t, s
+
+	def diff(self, initial, parent=None):
+		"""Take the diff from initial snapshot to this snapshot, and return it as a transaction.
+		If not given, parent defaults to self.transaction"""
+		actions = self._diff(initial.data, self.data)
+		return Transaction(self.transaction, actions)
+
+	def _diff(self, a, b, prefix=()):
+		"""Returns changes (prefix + path, old, new) from a to b.
+		Note that if a == b then we return [], and if a and b are incompatible we return
+		[(prefix, a, b)]"""
+		results = []
+		if isinstance(a, list) and isinstance(b, list):
+			for n, (old, new) in enumerate(zip(a, b)):
+				path = prefix + (n,)
+				results += self._diff(old, new, prefix=path)
+		elif isinstance(a, dict) and isinstance(b, dict):
+			for key in set(a)| set(b):
+				old = a.get(key, EMPTY)
+				new = b.get(key, EMPTY)
+				assert (a, b) != (EMPTY, EMPTY), "Key in neither dict, or EMPTY present in dict"
+				path = prefix + (key,)
+				results += self._diff(old, new, prefix=path)
+		else:
+			results = [(prefix, a, b)]
+		return results
